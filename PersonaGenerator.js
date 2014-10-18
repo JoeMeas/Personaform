61f29eb203ee2f4aa0675d37fb3d5814354b5bd5
JoeMeas <perrylinmeas@gmail.com>
2014-10-17 23:09:02 -0700

    This is a Commit

---
+++ PersonaGenerator.js
@@ -0,0 +1,50 @@
+
+(function(){
+
+	//Load  Facebook API
+	window.fbAsyncInit = function() {
+		FB.init({
+		appId      : '793098777421131',
+		xfbml      : true,
+		version    : 'v2.1'
+		status     : true
+		});
+	};
+
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	
+	//Handles Facebook login
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {
+			console.log('Logged in.');
+		}
+		else {
+			FB.login();
+		}
+	});
+	
+	//Make query to Facebook for JSON data with set fields
+	function jsonQuery(){
+		new Ajax.Request(url, 
+			{
+				method: "GET:,
+				onSucesss: ajaxSuccess,
+				onFailure: ajaxFailed,
+				onException: ajaxFailed
+			}
+		);
+	}
+	
+	//On successful JSON pull, do THINGS
+	function ajaxSucess(ajax){
+		var data = JSON.parse(ajax.responseText);
+	}
+
+	
+})();
