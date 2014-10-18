66ac9deb5662b309566a26af68b10bc3a06335e5
DanielAldridge <danielaldridge.inbox@gmail.com>
2014-10-18 14:38:12 -0700

    For submission

--- random_photo.js
+++ random_photo.js
@@ -1,15 +1,18 @@
 window.onload = function() {
-	$.ajax({
+/*	$.ajax({
   url: 'http://api.randomuser.me/',
   dataType: 'json',
   success: function(data){
     console.log(data);
 
- }})
+ }})*/
 
   // when submit is pressed
   var button = $("#submit");
   button.on("click", getDem );
+
+  var save = $("#save");
+  button.on("click", savePersona);
 
   function getDem() {
   	console.log("hello");
@@ -24,6 +27,12 @@ window.onload = function() {
   	var agemax = $("#agemax").val();
   }
   // try until age within range
+  function savePersona() {
+  	// rip html
+  	// write to save page
+  	// put image in user generated profiles
+  	
+  }
 
   function getUser(gender) {
   		gender = gender || "female";
@@ -35,9 +44,12 @@ window.onload = function() {
    		console.log()
 
    		$("#persona_pic").prop("src", data.results[0].user.picture.medium);
-   		$("")
-   		var age = 
-   		var occupation = 
+   		$("#big_name").html(data.results[0].user.name.first + " " +  data.results[0].user.name.last);
+   		$("#name").html(data.results[0].user.name.first + " " +  data.results[0].user.name.last);
+   		var age = new Date(parseInt(data.results[0].user.dob));
+   		$("#age").html(2014 - age.getFullYear());
+   		$("#gender").html(gender);
+   		/*var occupation =*/ 
    		
 
   }})
