6be4b61ed6b2569381dbddb993548af45f1b93e4
JoeMeas <perrylinmeas@gmail.com>
2014-10-18 03:37:41 -0700

    Changed JS file

--- PersonaGenerator.js
+++ PersonaGenerator.js
@@ -1,52 +1,33 @@
-
 (function(){
-	alert('taco poop');
 	
 	//Load  Facebook API
 	window.fbAsyncInit = function() {
-		FB.init({
-		appId      : '793098777421131',
-		xfbml      : true,
-		version    : 'v2.1'
-		});
+		FB.init({appId : '793098777421131', xfbml : true, version : 'v2.1'});
+				
+		FB.getLoginStatus(function(response){
+			FB.ui({method: 'share', href: 'http://localhost.com/'}, function(response){});
+			if(response.status === 'connected'){
+				FB.api('/me/friends', function (response) {
+					if (response && !response.error) {
+						console.log(response);
+						var dataDiv = document.getElementByID('data');
+						var data = document.createElement("div");
+						data.innerHTML = response;
+						dataDiv.appendChild(data);
+					}else{
+						FB.login();
+					}
+				});
+			}
+		})
 	};
 	
 	(function(d, s, id){
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) {return;}
 		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
+		js.src = "https://connect.facebook.net/en_US/sdk.js";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
-	
-	
-	//Handles Facebook login
-	FB.getLoginStatus(function(response) {
-		if (response.status === 'connected') {
-			console.log('Logged in.');
-		}
-		else {
-			FB.login();
-		}
-	});
-	
-	/*
-	//Make query to Facebook for JSON data with set fields
-	function jsonQuery(){
-		new Ajax.Request("https://graph.facebook.com/search?q=QUERY&type=OBJECT_TYPE", 
-			{
-				method: "GET:,
-				onSucesss: ajaxSuccess,
-				onFailure: ajaxFailed,
-				onException: ajaxFailed
-			}
-		);
-	}
-	
-	//On successful JSON pull, do THINGS
-	function ajaxSucess(ajax){
-		var data = JSON.parse(ajax.responseText);
-	}
-	*/
 	
 })();
